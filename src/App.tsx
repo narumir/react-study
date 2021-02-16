cdea415fe1da6899fa87fcf1c46a41b7abd2b08f
Jaeho Lee <openjacoblee@gmail.com>
2021-02-16 08:33:37 +0000

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {
+  FC,
+} from "react";
+
+export const App: FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hello
+      </header>
+    </div>
+  );
+};
